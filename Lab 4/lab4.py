18f5c8f639ce315e847e126844b54ce0a5bd344d
Mikołaj Galant <mikolaj.galant@gmail.com>
2024-03-20 09:54:09 +0100

    opracowanie metody iteracyjnego podstawienia

--- Lab 4/lab4.py
+++ Lab 4/lab4.py
@@ -1,8 +1,23 @@
-# Proszę zapoznać się z wykładem nr 5, w szczególności pomyśleć:
+# metoda iteracyjnego podstawienia
+import numpy as np
 
-#     jak zaimplentować metodę iteracyjnego podstawiania
-#     jak zaimplentować metodę Newtona-Raphsona (korzystając z rozwinięcia Taylor’a ograniczonego do pierwszej pochodnej oraz macierzy Jacobiego)
-#     zapoznać się z funkcjami scipy.optimize do szukania miejsc zerowych funkcji (np. bisekcji)
+def funkcja(x):
+    #x_i+1 = e^(-x_i)
+    return np.exp(-x)
 
-##pamiętaj o branch lab4 w repozytorium
+def iterative_substitution(ilosc_iteracji, x_0):
+    
+    x_i=[]
+    e = []
 
+    for i in range(ilosc_iteracji):
+        if i==0:
+            x_i.append(x_0)
+            e.append("")
+        else:
+            x_i.append(funkcja(x_i[i-1]))
+            e.append(abs(x_i[i]-x_i[i-1])/x_i[i]*100)
+    return x_i, e
+
+if __name__ == "__main__":
+    print(iterative_substitution(10, 0))
